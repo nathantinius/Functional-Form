9a6d3149ce31d37506b45d3027a0c574835d5757
Nathan Tinius <nathantinius@gmail.com>
2020-05-14 14:23:33 -0500

    MAJOR UPDATE: Payment Validation, ReadME, Refactoring, and Comments

---
+++ README.md
@@ -0,0 +1,11 @@
+# Interactive Form Project
+
+In this project I was tasked with providing a solid user experience in form navigation and validation. 
+
+To do this I provide a user experince with the following criteria:
+-   Hide conditional form fields until specific conditions were met
+-   Realtime form validation
+-   Disabling the Submit button until all validations are true
+-   When an activity is selected - disable activities with conflicting times
+-   Calculate the total cost
+
--- js/script.js
+++ js/script.js
@@ -1,26 +1,57 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 3 - Interactive Form
+******************************************/
+
+/**
+ * All Global Variables
+ */
 const getSubmit = document.querySelector('button');
 
+//Name Field and Regex
 const getName = document.getElementById('name');
-const nameRegixCheck = /^[A-Za-z]+\s?[A-Za-z]+$/;
+const nameRegexCheck = /^[a-z]+\s[a-z]+$/i;
 
+//Email Field and Regex
 const getEmail = document.getElementById('mail');
-const emailRegixCheck = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const emailRegexCheck = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i;
 
+//Job Title options and other field
 const getJobs = document.getElementById('title');
 const getBasicInfo = document.querySelector('.basic-info');
 const otherInput = document.getElementById('other-job-title');
+const jobRegexCheck = /^[a-z\s]+$/i;
 
+//T-shirt options
 const getShirtDesigns = document.getElementById('design');
 const getShirtColors = document.getElementById('color');
 
+//Activity checkboxes
 const getActivities = document.querySelector('.activities');
 const checkboxes = document.querySelectorAll(`input[type="checkbox"]`);
 
+//Payment fields and Regex
+const getPayment = document.getElementById('payment');
+const getCC = document.getElementById('credit-card');
+const getCCNum = document.getElementById('cc-num');
+const ccRegexCheck = /^[0-9]{4}\s?[0-9]{4}\s?[0-9]{4}\s?[0-9]{4}$/;
+const getZip = document.getElementById('zip');
+const zipRegexCheck = /^[0-9]{5}$/;
+const getCVV = document.getElementById('cvv');
+const cvvRegexCheck = /^[0-9]{3}$/;
+const getPP = document.getElementById('paypal');
+const getB = document.getElementById('bitcoin');
+
+//Tshirt option text placeholder used in showSelectTshirts()
 let optionText = "";
-//hide the other job title input on page load.
+
+//Hide all hidden options as default
 otherInput.style.display = 'none'
 getShirtColors.style.display = 'none';
-
+getCC.style.display = 'none';
+getPP.style.display = 'none';
+getB.style.display = 'none';
+getSubmit.disabled = true;
 
 /***
  * window.onload function sets the name field 
@@ -29,43 +60,50 @@ getShirtColors.style.display = 'none';
 window.onload = () => {
     getName.focus();
 }
-/***
- * Name Validation
- ***/
-function nameCheck() {
-    if (!nameRegixCheck.test(getName.value)) {
-        getName.style.border = "2px solid red";
-        getName.nextElementSibling.textContent = "Please input a valid name.";
-        formValid = false;
+
+/**
+ * Validation Functions
+ */
+
+function createListener(validator) {
+    return e => {
+        const text = e.target.value;
+        const valid = validator(text);
+        const showTip = text !== "" && !valid;
+        const tooltip = e.target.nextElementSibling;
+        showOrHideTip(showTip, tooltip);
+        formCheck();
+    };
+}
+
+function showOrHideTip(show, element) {
+    // show element when show is true, hide when false
+    if (show) {
+        element.style.display = "inherit";
     } else {
-        console.log(getName.value);
-        getName.style.border = '';
-        getName.nextElementSibling.textContent = "";
+        element.style.display = "none";
     }
 }
 
-getName.addEventListener('blur', () => {
-    nameCheck();
-})
-
 /***
- * Email Validation
+ * Basic Info Validations and Event Listeners
  ***/
-function emailCheck() {
-    if (!emailRegixCheck.test(getEmail.value)) {
-        getEmail.style.border = "2px solid red";
-        getEmail.nextElementSibling.textContent = "Please input a valid email.";
-        formValid = false;
-    } else {
-        console.log(getEmail.value);
-        getEmail.style.border = '';
-        getEmail.nextElementSibling.textContent = "";
-    }
+
+function nameCheck(name) {
+    return nameRegexCheck.test(name)
 }
 
-getEmail.addEventListener('blur', () => {
-    emailCheck();
-})
+function emailCheck(email) {
+    return emailRegexCheck.test(email)
+}
+
+function jobCheck() {
+    return jobRegexCheck.test(otherInput.value)
+}
+
+getName.addEventListener('input', createListener(nameCheck));
+getEmail.addEventListener('input', createListener(emailCheck));
+otherInput.addEventListener('input', createListener(jobCheck));
 
 /***
  * Looks at the Job Title Selected 
@@ -80,7 +118,7 @@ function showOtherJobOption() {
     }
 }
 
-document.getElementById('title').addEventListener('change', () => {
+getJobs.addEventListener('change', () => {
     showOtherJobOption();
 })
 
@@ -114,6 +152,7 @@ function showSelectTshirts() {
             } else {
                 getShirtColors.options[i].disabled = false;
                 getShirtColors.selectedIndex = 3;
+
             }
         }
     }
@@ -134,7 +173,6 @@ getActivities.appendChild(total);
 
 
 function checkCheckboxes(checked, itemCost, chosenWorkshopTime) {
-    console.log(checked.value);
     if (checked.checked) {
         totalCost += itemCost;
     } else {
@@ -144,8 +182,6 @@ function checkCheckboxes(checked, itemCost, chosenWorkshopTime) {
     checkboxes.forEach((checkbox) => {
         const otherWorkshopTime = checkbox.getAttribute('data-day-and-time');
         if (checked.checked && chosenWorkshopTime == otherWorkshopTime) {
-            console.log(chosenWorkshopTime);
-            console.log(otherWorkshopTime);
             if (checkbox !== checked) {
                 checkbox.disabled = true;
             }
@@ -163,12 +199,87 @@ checkboxes.forEach((checkbox) => {
         const itemCost = parseInt(event.target.getAttribute('data-cost'));
         const chosenWorkshopTime = event.target.getAttribute('data-day-and-time');
         checkCheckboxes(checked, itemCost, chosenWorkshopTime);
+        formCheck();
     })
 })
 
-
+function activitiesCheck() {
+    if (totalCost > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 /***
- * Disable Activities that conflict.
+ * Show Payment Option based on what option is selected.
  ***/
+function showPaymentOption() {
+    if (getPayment.options[getPayment.selectedIndex].value == 'credit card') {
+        getCC.style.display = 'block';
+        getPP.style.display = 'none';
+        getB.style.display = 'none';
+    } else if (getPayment.options[getPayment.selectedIndex].value == 'paypal') {
+        getCC.style.display = 'none';
+        getPP.style.display = 'block';
+        getB.style.display = 'none';
+    } else if (getPayment.options[getPayment.selectedIndex].value == 'bitcoin') {
+        getCC.style.display = 'none';
+        getPP.style.display = 'none';
+        getB.style.display = 'block';
+    } else {
+        getCC.style.display = 'none';
+        getPP.style.display = 'none';
+        getB.style.display = 'none';
+    }
+}
 
+/**
+ * Payment Validation and Event Listeners
+ */
+
+function paymentCheck() {
+    if (getPayment.options[getPayment.selectedIndex].value == 'select method') {
+        return false;
+    } else if (getPayment.options[getPayment.selectedIndex].value == 'credit card') {
+        if (ccNumberCheck(getCCNum.value) && zipCheck(getZip.value) && cvvCheck(getCVV.value)) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return true;
+    }
+}
+
+function ccNumberCheck() {
+    return ccRegexCheck.test(getCCNum.value);
+}
+function zipCheck() {
+    return zipRegexCheck.test(getZip.value);
+}
+function cvvCheck() {
+    return cvvRegexCheck.test(getCVV.value);
+}
+
+getCCNum.addEventListener('input', createListener(ccNumberCheck));
+getZip.addEventListener('input', createListener(zipCheck));
+getCVV.addEventListener('input', createListener(cvvCheck));
+
+getPayment.addEventListener('change', () => {
+    showPaymentOption();
+    formCheck();
+})
+
+/**
+ * Form Check and Submit Button activation
+ */
+
+function formCheck() {
+    if (nameCheck(getName.value) && emailCheck(getEmail.value) && activitiesCheck() && paymentCheck()) {
+        getSubmit.disabled = false;
+    } else {
+        getSubmit.disabled = true;
+
+    }
+}
